1564055ed2080eb84b3a011af0a14e8efc7715b3
cduong13 <chris.duong83@gmail.com>
2016-12-23 15:39:31 +0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# zabbix_agent_lite
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'zabbix_agent_lite'
+maintainer 'cduong13'
+maintainer_email 'chris.duong83@gmail.com'
+license 'mit'
+description 'Installs/Configures zabbix_agent_lite'
+long_description 'Installs/Configures zabbix_agent_lite'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/zabbix_agent_lite/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/zabbix_agent_lite' if respond_to?(:source_url)
