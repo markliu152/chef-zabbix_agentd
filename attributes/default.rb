be8b69245d79ed1cf767387db14780f157db3e24
cduong13 <chris.duong83@gmail.com>
2016-12-26 18:19:31 +0700

    define attrs for YUM 2.2 and 3.2 only

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,6 +16,56 @@ default['zabbix_agentd']['repo_rpm_url'] = {
           'url' => 'http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/',
           'package' => 'zabbix-release-3.2-1.el7.noarch.rpm'
         }
+      },
+      '6' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/3.2/rhel/6/x86_64/',
+          'package' => 'zabbix-release-3.2-1.el6.noarch.rpm'
+        }
+      }
+    },
+    'redhat' => {
+      '7' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/',
+          'package' => 'zabbix-release-3.2-1.el7.noarch.rpm'
+        }
+      },
+      '6' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/3.2/rhel/6/x86_64/',
+          'package' => 'zabbix-release-3.2-1.el6.noarch.rpm'
+        }
+      }
+    }
+  },
+  '2.2' => {
+    'centos' => {
+      '7' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/2.2/rhel/7/x86_64/',
+          'package' => 'zabbix-release-2.2-1.el7.noarch.rpm'
+        }
+      },
+      '6' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/',
+          'package' => 'zabbix-release-2.2-1.el6.noarch.rpm'
+        }
+      }
+    },
+    'redhat' => {
+      '7' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/2.2/rhel/7/x86_64/',
+          'package' => 'zabbix-release-2.2-1.el7.noarch.rpm'
+        }
+      },
+      '6' => {
+        'x86_64' => {
+          'url' => 'http://repo.zabbix.com/zabbix/2.2/rhel/6/x86_64/',
+          'package' => 'zabbix-release-2.2-1.el6.noarch.rpm'
+        }
       }
     }
   }
