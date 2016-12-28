1ca170dfc26e841a4b50736a0bf973ba0cc11713
cduong13 <chris.duong83@gmail.com>
2016-12-28 17:04:57 +0700

    configure apt release for Ubuntu

--- recipes/apt_repo.rb
+++ recipes/apt_repo.rb
@@ -7,3 +7,16 @@
 # Copyright (c) 2016 cduong13
 
 include_recipe 'apt::default'
+
+zabbix_release = node['zabbix_agentd']['version'].match(/\d*\.\d*/).to_s
+
+case node['platform']
+when 'ubuntu'
+  apt_repository 'repo.zabbix.com' do
+    uri "https://repo.zabbix.com/zabbix/#{zabbix_release}/ubuntu"
+    distribution node['lsb']['codename']
+    components ['main']
+    key 'https://repo.zabbix.com/zabbix-official-repo.key'
+    action :add
+  end
+end
